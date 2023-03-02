1637b14b9cf9cae98ec9d8e458599f2ecef8af4d
Fumiya Kito <fumiya@mba.local>
2023-03-02 20:40:34 +0900

    feat: NewApplication Form, POST API, automatically fetch

--- src/components/ApplicaionList.tsx
+++ src/components/ApplicaionList.tsx
@@ -1,10 +1,13 @@
 import ApplicaionItem from "./ApplicationItem";
 import Application from "../models/application";
 
+
 const ApplicationList: React.FC<{ applications: Application[] }> = (props) => {
-  return <ul>
-    { props.applications.map(item => <ApplicaionItem key={item.id} name={item.name} />) }
-  </ul>
+  return <>
+    <ul>
+      { props.applications.map(item => <ApplicaionItem key={item.id} name={item.name} />) }
+    </ul>
+  </>
 }
 
 
---
+++ src/components/NewApplication.tsx
@@ -0,0 +1,46 @@
+import { useRef } from 'react';
+import useHttp from '../hooks/use-http';
+// import { ApplicationContext } from '../store/application-context';
+
+const NewApplication: React.FC<{ onPostSuccess: () => void }> = (props) => {
+  // const applicationCtx = useContext(ApplicationContext)
+  const { isLoading, error, sendRequest } = useHttp()
+  const nameInputRef = useRef<HTMLInputElement>(null)
+
+
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    const enteredName = nameInputRef.current!.value
+
+    if (enteredName.trim().length === 0) {
+      return;
+    }
+
+    sendRequest({ 
+      url: 'https://react-http-eb5ad-default-rtdb.firebaseio.com/applications.json',
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: { name: enteredName }
+    })
+
+    if (!error) {
+      nameInputRef.current!.value = ''
+    }
+    setTimeout(props.onPostSuccess, 750)
+    
+  }
+
+
+  return  <>
+    {isLoading ? <p>Pending...</p>
+    :
+    <form onSubmit={submitHandler}>
+      <label htmlFor='text'>Your Name</label>
+      <input type='text' id='name' ref={nameInputRef}/>
+      <button>Add Application</button> 
+    </form>
+    }
+  </>
+}
+
+export default NewApplication;
